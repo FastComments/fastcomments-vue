df1ba5b2fa2484f799940860f9c954a56dde20a1
winrid <winrid@gmail.com>
2025-03-17 19:08:46 -0700

    Adding spinner example

---
+++ dev/serve-spinner.vue
@@ -0,0 +1,35 @@
+<script lang="ts">
+import Vue from 'vue';
+import FastComments from '@/entry';
+
+export default Vue.extend({
+  name: 'ServeDev',
+  components: {
+    FastComments
+  },
+  data() {
+    return {
+      showSpinner: true
+    }
+  },
+  computed: {
+    fastCommentsConfig() {
+      return {
+        tenantId: 'demo',
+        urlId: 'test',
+        onRender: () => { this.showSpinner = false }
+      }
+    }
+  }
+});
+
+</script>
+
+<template>
+  <div id="app">
+    <div v-if="showSpinner">
+      Spinner here...
+    </div>
+    <fast-comments v-bind:config="fastCommentsConfig"/>
+  </div>
+</template>
